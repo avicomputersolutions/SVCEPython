0a00a4c7ea39031ab6e7facb3463b6bfc561c581
avicomputersolutions <30495311+avicomputersolutions@users.noreply.github.com>
2025-03-05 19:56:34 +0530

    upp

---
+++ postive.java
@@ -0,0 +1,11 @@
+public class postive{
+    public static  void  main(String[] arg){
+            int a =7;
+            if( a> 0){
+                System.out.println("postive");
+            }
+            else{ 
+                System.out.println("Negative");
+            }
+    }
+}
